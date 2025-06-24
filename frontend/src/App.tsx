d3b1a95cebe2a2303c91aefe2edb8771ab7e96f8
thiago_vieira2 <ra58029747888@acaonsfatima.org.br>
2025-06-24 09:13:29 -0300

    dwd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,39 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
 
+  const [peso, setPeso] = useState(''); 
+  const [Valor, setValor] = useState('');
+  const [declarado, setDeclarado] = useState('');
+  const [markup, setMarkup]= useState('');
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main className="bg-white w-screen h-screen">
+
+      <div className="w-full flex justify-center mt-6">
+        <h1 className="text-black text-4xl">
+          Sistema de importação
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center h-[calc(100vh-100px)]">
+        <div className="flex flex-col items-center gap-4">
+          <input type="text" className="border border-zinc-300 p-2 rounded-md w-72" placeholder="Peso (g)" />
+          <input type="text" className="border border-zinc-300 p-2 rounded-md w-72" placeholder="Valor convertido (¥)" />
+          <input type="text" className="border border-zinc-300 p-2 rounded-md w-72" placeholder="Declarado (USD)" />
+          <input type="text" className="border border-zinc-300 p-2 rounded-md w-72" placeholder="Markup (%)" />
+
+          <button className="bg-black text-white p-2 rounded-md w-72 hover:bg-zinc-800 transition">
+            Calcular
+          </button>
+        </div>
+      </div>
+
+    </main>
+
   )
 }
 
